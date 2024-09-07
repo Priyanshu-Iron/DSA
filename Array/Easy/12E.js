a8a32a051d81b170de07eb8fdf0457b904b1e0a7
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2024-09-07 20:44:27 +0530

    Optimal Solution Added

--- Array/Easy/12E.js
+++ Array/Easy/12E.js
@@ -2,7 +2,7 @@
 
 // Brute Force Method
 let arr1 = [1,2,3,4,5];
-let arr2 = [1,2,3,4];
+let arr2 = [1,2,3,4,6];
 
 function FindUnion (arr1,arr2) {
     
@@ -26,13 +26,40 @@ function FindUnion (arr1,arr2) {
 // FindUnion(arr1,arr2);
 
 function SortedUnionArray (arr1,arr2) {
-    while (i<arr1.length && i<arr2.length) {
+    
+    let i = 0;
+    let j = 0;
+    let UnionArray = [];
+
+    while (i<arr1.length && j<arr2.length) {
         if (arr1[i] <= arr2[j]) {
-            if (UnionArray.back() != arr1[i]) {
-                UnionArray.push_back(arr1[i]);
+            if (UnionArray.length === 0 || UnionArray[UnionArray.length-1] !== arr1[i]) {
+                UnionArray.push(arr1[i]);
             }
             i++;
+        } else{
+            if (UnionArray.length === 0 || UnionArray[UnionArray.length-1] !== arr2[j]) {
+                UnionArray.push(arr2[j]);
+            }
+            j++;
         }
     }
+    
+    while (i<arr1.length) {
+        if (UnionArray.length === 0 || UnionArray[UnionArray.length-1] !== arr1[i]) {
+            UnionArray.push(arr1[i]);
+        }
+        i++;
+    }
+
+    while(j<arr2.length){
+        if (UnionArray.length === 0 || UnionArray[UnionArray.length-1] !== arr2[j]) {
+            UnionArray.push(arr2[j]);
+        }
+        j++;
+    }
+
     return UnionArray;
 }
+
+console.log(`Union of Two Sorted Array is ${SortedUnionArray(arr1,arr2)}`);
