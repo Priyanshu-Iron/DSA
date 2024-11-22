af34c807630fa0c512e7d23ad063273dcd3c5c8e
Priyanshu Singh <priyanshu@Priyanshus-MacBook-Air.local>
2024-11-22 10:24:09 +0530

    setMatrix0

--- Array/Medium/setMatrix0.js
+++ Array/Medium/setMatrix0.js
@@ -1,55 +1,73 @@
 // node setMatrix0.js
-function markRow(matrix, n, m, i) {
-    // set all non-zero elements as -1 in the row i:
-    for (let j = 0; j < m; j++) {
-      if (matrix[i][j] !== 0) {
-        matrix[i][j] = -1;
-      }
-    }
-  }
+
+var setZeroes = function(matrix) {
+  let n = matrix.length;
+  let m = matrix[0].length;
   
-  function markCol(matrix, n, m, j) {
-    // set all non-zero elements as -1 in the col j:
-    for (let i = 0; i < n; i++) {
-      if (matrix[i][j] !== 0) {
-        matrix[i][j] = -1;
-      }
+  let zeroFirstRow = false; // Flag to mark if the first row should be zeroed
+  let zeroFirstCol = false; // Flag to mark if the first column should be zeroed
+
+  // Check if the first row contains any zero
+  for (let j = 0; j < m; j++) {
+    if (matrix[0][j] === 0) {
+      zeroFirstRow = true;
+      break;
     }
-  }
-  
-  function zeroMatrix(matrix, n, m) {
-    // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < m; j++) {
-        if (matrix[i][j] === 0) {
-          markRow(matrix, n, m, i);
-          markCol(matrix, n, m, j);
-        }
-      }
-    }
-    // Finally, mark all -1 as 0:
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < m; j++) {
-        if (matrix[i][j] === -1) {
-          matrix[i][j] = 0;
-        }
-      }
-    }
-    return matrix;
   }
 
-  let matrix = [
-    [1,1,1],
-    [1,0,1],
-    [1,1,1]
-];
-  
-  const n = matrix.length;
-  const m = matrix[0].length;
-  
-  const ans = zeroMatrix(matrix, n, m);
-  
-  console.log("The Final matrix is: ");
+  // Check if the first column contains any zero
   for (let i = 0; i < n; i++) {
-    console.log(ans[i].join(" "));
+    if (matrix[i][0] === 0) {
+      zeroFirstCol = true;
+      break;
+    }
   }
+
+  // Use the first row and first column to mark zeros for other cells
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0; // Mark the start of the row
+        matrix[0][j] = 0; // Mark the start of the column
+      }
+    }
+  }
+
+  // Zero out cells based on markers in the first row and first column
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < m; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  // Zero the first row if needed
+  if (zeroFirstRow) {
+    for (let j = 0; j < m; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  // Zero the first column if needed
+  if (zeroFirstCol) {
+    for (let i = 0; i < n; i++) {
+      matrix[i][0] = 0;
+    }
+  }
+
+  return matrix;
+};
+
+let matrix = [
+  [1, 1, 1],
+  [1, 0, 1],
+  [1, 1, 1]
+];
+
+const ans = setZeroes(matrix);
+
+console.log("The Final matrix is: ");
+for (let i = 0; i < ans.length; i++) {
+  console.log(ans[i].join(" "));
+}
