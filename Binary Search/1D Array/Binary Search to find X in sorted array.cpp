76d220168798ae837140fba2d93754ad61f2b361
Priyanshu-Iron <ps2894944@gmail.com>
2025-07-30 22:51:55 +0530

    Binary Search

---
+++ Binary Search/1D Array/Binary Search to find X in sorted array.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+ #include <iostream>
+ 
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int n, target;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    cout << "Enter target to search: ";
+    cin >> target;
+    int result = search(nums, target);
+    if (result != -1)
+        cout << "Target found at index: " << result << endl;
+    else
+        cout << "Target not found in array." << endl;
+    return 0;
+}
